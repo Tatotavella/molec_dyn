a6bd28cb292fae93aaa22b7081782c0921f36d14
Lucas Danilo Longo <lucaslongo52@gmail.com>
2017-06-30 11:14:48 -0300

    "Agregado de grafico H-Boltzman"

--- bin/outgraph.py
+++ bin/outgraph.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 m = np.loadtxt("out.csv",delimiter=",")
+hb = np.loadtxt("HB.csv",delimiter=",")
+
 step = m[:,0]
+stephb=hb[:,0]
 ordver = m[:,1]
 vx = m[:,2]
 vy = m[:,3]
@@ -11,22 +14,32 @@ fy = m[:,6]
 fz = m[:,7]
 ekin = m[:,8]
 epot = m[:,9]
+hboltz = hb[:, 1]
 
+plt.figure(1)
 plt.plot(step,vx,'r')
 plt.plot(step,vy,'g')
 plt.plot(step,vz,'b')
 plt.title("Velocidades")
-plt.show()
+
+plt.figure(2)
 plt.plot(step,fx,'r')
 plt.plot(step,fy,'g')
 plt.plot(step,fz,'b')
 plt.title("Fuerzas")
-plt.show()
+
+plt.figure(3)
 plt.plot(step,ordver,'r')
 plt.title("Ordenamiento Verlet")
-plt.show()
+
+plt.figure(4)
 plt.plot(step,ekin,'r')
 plt.plot(step,epot,'g')
 plt.plot(step,ekin+epot,'b')
 plt.title("Energia")
+
+plt.figure(5)
+plt.plot(stephb,hboltz,'g')
+plt.title("H-Boltzman")
 plt.show()
+
