a9dc97898d52a67d519ca37e9557e77a3acc0f77
Lucas Danilo Longo <lucaslongo52@gmail.com>
2017-07-06 21:28:26 -0300

    "Lindemann index funcionando"

--- bin/lindeman.py
+++ bin/lindeman.py
@@ -1,13 +1,13 @@
-
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
 
-traject = np.loadtxt("trajectories_1.000000_data.csv",delimiter=",")
+traject = np.loadtxt("T_2-ro_0.1.csv",delimiter=",")
 n=4
 N=n*n*n
-nsamples=1000
+nsamples=500
+
 
 #diccionario de matrices Rij[time]
 
@@ -19,12 +19,25 @@ for time in range(0,nsamples):
 #lleno la matriz rij del tiempo time
  for i in range(0,N):
   for j in range (0,i):
-   dx=traject[i+(N-1)*time,1]-traject[j+(N-1)*time,1]
-   dy=traject[i+(N-1)*time,2]-traject[j+(N-1)*time,2]
-   dz=traject[i+(N-1)*time,3]-traject[j+(N-1)*time,3]
-   rij[i,j]=np.sqrt(dx*dx+dy*dy+dz*dz) #elemento ij de la matriz
+   dx=traject[i+N*time,1]-traject[j+N*time,1]
+   dy=traject[i+N*time,2]-traject[j+N*time,2]
+   dz=traject[i+N*time,3]-traject[j+N*time,3]
+   rij[i,j]=np.sqrt((dx*dx)+(dy*dy)+(dz*dz)) #elemento ij de la matriz
  Rij[time]=rij #guardo la matriz
 
+
+#matriz lindeman de pares:
+ L={}
+#En la posicion ij de L[time] va a contener [rij(time)-rij(0)/rij(0)]^2
+
+#recorro los tiempos
+for time in range(0,nsamples):
+ lij=np.zeros((N,N))    
+#lleno la matriz rij del tiempo time
+ for i in range(0,N):
+  for j in range (0,i):
+   lij[i,j]=((Rij[time][i,j]-Rij[0][i,j])/(Rij[0][i,j]))**2#elemento ij de la matriz
+ L[time]=lij #guardo la matriz
 
 #Matriz de promedios
 Prom_Rij={}
@@ -37,46 +50,33 @@ for time in range(0,nsamples):
    promrij[i,j]=0 #elemento ij de la matriz
  Prom_Rij[time]=promrij #guardo la matriz
 
-#Matriz de promedios cuad
-Prom_Rijcuad={}
-
-#lleno Prom_Rijcuad con matrices nulas de NxN
-for time in range(0,nsamples):
- promrij=np.zeros((N,N))    
- for i in range(0,N):
-  for j in range (0,i):
-   promrij[i,j]=0 #elemento ij de la matriz
- Prom_Rijcuad[time]=promrij #guardo la matriz
-
-
-#lleno Prom_Rij y Prom_Rijcuad
+#lleno Prom_Rij
 suma=0
 sumacuad=0
 for i in range(0,N):
  for j in range(0,i):
   for time in range(0,nsamples):
-    suma=suma+Rij[time][i,j]
-    sumacuad=sumacuad+(Rij[time][i,j]*Rij[time][i,j])
+    suma=suma+L[time][i,j]
     promedio=float(suma)/float(1+time)
-    promediocuad=float(sumacuad)/float(1+time)
     Prom_Rij[time][i,j]=promedio
-    Prom_Rijcuad[time][i,j]=promediocuad
   suma=0
-  sumacuad=0
+  sumacuad=0 
 
-
-#Ahora calculo el lindeman del tiempo time
-liendeman=[]
+#Ahora calculo el lindeman del tiempo time del solido
+liendemang=[]
 
 l=0
 for time in range(0,nsamples): 
  for i in range(0,N):
   for j in range(0,i):
-    l=l+np.sqrt(np.abs(Prom_Rijcuad[time][i,j]-(Prom_Rij[time][i,j]*Prom_Rij[time][i,j])))/(Prom_Rij[time][i,j])   
- liendeman.append([time,l*2/(N*(N-1))])  
- l=0  
+    l=l+Prom_Rij[time][i,j]                        #sumo
+ liendemang.append([time,np.sqrt(l*2/(N*(N-1)))])  #promedio en el total de intercc N(N-1)/2 y raiz cuadrada
+ l=0
 
 #grafico
 for time in range(0,nsamples):
- plt.plot(liendeman[time][0],liendeman[time][1],'go')  
+ plt.plot(liendemang[time][0],liendemang[time][1],'ro')
 plt.show()
+
+
+
